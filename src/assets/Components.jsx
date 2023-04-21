0236033f3402a1e82c6d9e92e9570b1e8b21091e
Eryk Bartoszewicz <58864390+Frod00@users.noreply.github.com>
2023-04-21 09:29:12 +0200

    components

---
+++ src/assets/Components.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function NewVote(props){
+    return(
+        <button>
+            <h2>
+                Nowe głosowanie
+            </h2>
+        </button>
+
+    )
+}
+function YourVote(props){
+    return(
+        <button2>
+            <h3>
+                Twoje głosowanie
+            </h3>
+        </button2>
+
+
+    )
+}
+
+
+export {NewVote, YourVote};
